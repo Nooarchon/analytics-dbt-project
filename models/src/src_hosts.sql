1f3d40d90d483e8815e1e0552f9aacde49576328
Julija Melik-Sachnazarova <j.melik.sachnazarova@accenture.com>
2026-07-06 11:55:49 +0000

    Chaged

---
+++ models/src/src_hosts.sql
@@ -0,0 +1,17 @@
+with raw_hosts as (
+     select *
+    from AIRBNB.RAW.RAW_HOSTS
+
+),
+final as (
+    select
+        id as host_id,
+        name as host_name,
+        is_superhost,
+        created_at,
+        updated_at
+    from raw_hosts
+)
+
+select *
+from final

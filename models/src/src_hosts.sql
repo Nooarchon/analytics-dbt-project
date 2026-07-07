122f28d1e711cf897ba5513e6c87683b944655cc
Julija Melik-Sachnazarova <j.melik.sachnazarova@accenture.com>
2026-07-07 08:32:34 +0000

    change

--- models/src/src_hosts.sql
+++ models/src/src_hosts.sql
@@ -1,17 +1,9 @@
-with raw_hosts as (
-     select *
-    from AIRBNB.RAW.RAW_HOSTS
+with raw_customers as (
 
-),
-final as (
-    select
-        id as host_id,
-        name as host_name,
-        is_superhost,
-        created_at,
-        updated_at
-    from raw_hosts
+    select *
+    from RAW.JAFFLE_SHOP.CUSTOMERS
+
 )
 
 select *
-from final
+from raw_customers
